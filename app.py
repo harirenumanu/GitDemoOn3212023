8c93d9f93b328bfc842ea960d1cb1960785984ce
Hari <hrenumanu@gmail.com>
2025-01-07 03:28:48 -0500

    demo

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
-Hell this is hari i am changing some changes
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-Hello changes are happening am i giht 
+class HelloHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"Hello, World!")
+
+def run(server_class=HTTPServer, handler_class=HelloHandler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd server on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
